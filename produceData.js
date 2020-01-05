db5c579e2a7ca757c153a6cce3d69941b4412607
smukhiyabeen <subarna.karki13@gmail.com>
2020-01-05 00:39:09 -0500

    update lint rules and apply lint

--- produceData.js
+++ produceData.js
@@ -1,27 +1,27 @@
 const produceData = [
-    {
-        name: 'lettuce',
-        code: 'A12T-4GH7-QPL9-3N4M',
-        price: 3.46
-    },
-    {
-        name: 'peach',
-        code: 'E5T6-9UI3-TH15-QR88',
-        price: 2.99
-    },
-    {
-        name: 'green pepper',
-        code: 'YRT6-72AS-K736-L4AR',
-        price: 0.79
-    },
-    {
-        name: 'gala apple',
-        code: 'TQ4C-VV6T-75ZX-1RMR',
-        price: 3.59
-    }
+  {
+    name: 'lettuce',
+    code: 'A12T-4GH7-QPL9-3N4M',
+    price: 3.46,
+  },
+  {
+    name: 'peach',
+    code: 'E5T6-9UI3-TH15-QR88',
+    price: 2.99,
+  },
+  {
+    name: 'green pepper',
+    code: 'YRT6-72AS-K736-L4AR',
+    price: 0.79,
+  },
+  {
+    name: 'gala apple',
+    code: 'TQ4C-VV6T-75ZX-1RMR',
+    price: 3.59,
+  },
 ];
 
 
 module.exports = {
-    produceData
-}
+  produceData,
+};
